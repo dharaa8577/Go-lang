b8dbe62ae8c16680dfff0662aa0dfa3d72962c8a
dharaa8577 <121138188+dharaa8577@users.noreply.github.com>
2022-12-22 13:27:39 -0600

    calculator

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a int
+		b int
+	)
+	a = 10
+	b = 20
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+
+}
